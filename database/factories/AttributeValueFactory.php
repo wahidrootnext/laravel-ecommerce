03ab19e73efe29b3306d34c60e7312464c1952b9
Mohammad Wahid <m.wahid@rootnext.com>
2021-06-23 21:38:06 +0600

    Database model, migration(empty), factory, seeder & controller added

---
+++ database/factories/AttributeValueFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\AttributeValue;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AttributeValueFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = AttributeValue::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
